57f45efbec0189f186f3df10ba6ccdeb8e0d4f2c
Arnaldo Victor <arnaldovictorm@gmail.com>
2023-09-18 01:18:49 -0300

    :bug: fix: create macro only after loads from github

--- src/utils/lua/enemy.lua
+++ src/utils/lua/enemy.lua
@@ -28,50 +28,54 @@ function table_merge(...)
 end
 
 friendList = {}
+enemyMacro = nil
 
 function callback(data, err)
     if err then
         return
     end
 
-    friendList = table_merge(data.novaHolanda, data.sideBySide, data.makers)
+    cpList = table_merge(data.novaHolanda, data.sideBySide, data.makers)
+
+    for index, value in ipairs(cpList) do
+        friendList[value:lower()] = true
+    end
+
+    enemyList = {'*', 'piriguete'}
+
+    for index, value in ipairs(enemyList) do
+        enemyList[value:lower():trim()] = true
+        enemyList[index] = nil
+    end
+
+    enemyMacro = macro(100, 'Enemy', function()
+        local pos = pos()
+        local actualTarget
+        for _, creature in ipairs(getSpectators(pos)) do
+            local specHp = creature:getHealthPercent()
+            local specPos = creature:getPosition()
+            local specName = creature:getName():lower()
+            if creature:isPlayer() and specHp and specHp > 0 then
+                if (not friendList[specName] and creature:getEmblem() ~= 1 and creature:getShield() < 3 and creature ~= player) or enemyList[specName] then
+                    if creature:canShoot() then
+                        if not actualTarget or actualTargetHp > specHp or (actualTargetHp == specHp and getDistanceBetween(pos, actualTargetPos) > getDistanceBetween(specPos, pos)) then
+                            actualTarget, actualTargetPos, actualTargetHp = creature, specPos, specHp
+                        end
+                    end
+                end
+            end
+        end
+        if actualTarget and g_game.getAttackingCreature() ~= actualTarget then
+            modules.game_interface.processMouseAction(nil, 2, pos, nil, actualTarget, actualTarget)
+        end
+    end)
+    
+    enemyIcon = addIcon("Enemy", {item =3292, text = "Enemy"}, enemyMacro )
+    enemyIcon:breakAnchors()
+    enemyIcon:move(80, 40)
+
 
 end
 
 HTTP.getJSON("https://raw.githubusercontent.com/ArnaldoVictor/nova_holanda/master/src/data/friends.json", callback)
 
-enemyList = {'*', 'piriguete'}
-
-for index, value in ipairs(enemyList) do
-    enemyList[value:lower():trim()] = true
-    enemyList[index] = nil
-end
-
-
-for index, value in ipairs(friendList) do
-    friendList[value:lower():trim()] = true
-    friendList[index] = nil
-end
-
-
-macro(100, 'Enemy', function()
-    local pos = pos()
-    local actualTarget
-    for _, creature in ipairs(getSpectators(pos)) do
-        local specHp = creature:getHealthPercent()
-        local specPos = creature:getPosition()
-        local specName = creature:getName():lower()
-        if creature:isPlayer() and specHp and specHp > 0 then
-            if (not friendList[specName] and creature:getEmblem() ~= 1 and creature:getShield() < 3 and creature ~= player) or enemyList[specName] then
-                if creature:canShoot() then
-                    if not actualTarget or actualTargetHp > specHp or (actualTargetHp == specHp and getDistanceBetween(pos, actualTargetPos) > getDistanceBetween(specPos, pos)) then
-                        actualTarget, actualTargetPos, actualTargetHp = creature, specPos, specHp
-                    end
-                end
-            end
-        end
-    end
-    if actualTarget and g_game.getAttackingCreature() ~= actualTarget then
-        modules.game_interface.processMouseAction(nil, 2, pos, nil, actualTarget, actualTarget)
-    end
-end)
